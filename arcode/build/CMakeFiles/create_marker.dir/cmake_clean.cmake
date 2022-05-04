785f4d16568d99f2bb2643faa9ba6e3bf8994d31
hamidrezafahimi <hamidrfahimi@gmail.com>
2022-05-04 16:23:17 +0430

    Aruco marker generation scripts addes

---
+++ arcode/build/CMakeFiles/create_marker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/create_marker.dir/create_marker.cpp.o"
+  "create_marker.pdb"
+  "create_marker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/create_marker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
